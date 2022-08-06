2e0e5cf7a8e5a9f2ddb22dbeab8ba805ae44f71a
aaustin07 <aaustin07@outlook.com>
2022-08-06 12:24:10 -0500

    finished 10..on 11

--- Scripts.sql
+++ Scripts.sql
@@ -17,10 +17,57 @@ FROM data_analyst_jobs
 -- FROM data_analyst_jobs 
 -- WHERE locations = 'TN'
 -- AND star_rating > 4
---4
+--4 - 3 rows
 
--- Select *
+-- Select
+-- title
 -- FROM data_analyst_jobs
 -- WHERE review_count BETWEEN 500 AND 1000
---5
+--5). 151 rows
+
+-- SELECT 
+-- locations,
+-- AVG(star_rating) as Avg_rating
+-- FROM data_analyst_jobs
+-- WHERE star_rating IS NOT NULL --help from Alex
+-- GROUP BY 1
+-- ORDER BY Avg_rating DESC
+--6). 46 rows; NE
+
+-- SELECT DISTINCT
+-- title
+-- FROM data_analyst_jobs
+--7). 881 rows
+
+-- SELECT DISTINCT
+-- title,
+-- locations
+-- FROM data_analyst_jobs
+-- WHERE locations = 'CA'
+--8). 230 rows 
+
+-- SELECT 
+-- company,
+-- AVG(star_rating) AS avg_star
+-- FROM data_analyst_jobs
+-- WHERE company IS NOT NULL --help from Alex
+-- AND review_count > 5000
+-- GROUP BY 1
+--9) 40 rows
+
+-- SELECT 
+-- company,
+-- AVG(star_rating) AS avg_star
+-- FROM data_analyst_jobs
+-- WHERE company IS NOT NULL --help from Alex
+-- AND review_count > 5000
+-- GROUP BY 1
+-- ORDER BY avg_star DESC
+--10) 40 rows, Unilever
+
+-- SELECT DISTINCT
+-- title 
+-- FROM data
+
+
 
