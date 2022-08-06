1a27ee3d8fa183e548e3ce80ef7dfb06d1939d4b
aaustin07 <aaustin07@outlook.com>
2022-08-06 11:26:21 -0500

    on question 5

---
+++ Scripts.sql
@@ -0,0 +1,26 @@
+/*SELECT Count(*)
+FROM data_analyst_jobs
+1).1793*/
+
+-- SELECT *
+-- FROM data_analyst_jobs
+-- LIMIT 10
+-- 2). ExxonMobile
+
+-- SELECT *
+-- FROM data_analyst_jobs
+-- WHERE locations IN ('TN', 'KY')
+--3). total 27 rows; 21 rows in TN; 6 rows in KY
+
+-- SELECT 
+-- title
+-- FROM data_analyst_jobs 
+-- WHERE locations = 'TN'
+-- AND star_rating > 4
+--4
+
+-- Select *
+-- FROM data_analyst_jobs
+-- WHERE review_count BETWEEN 500 AND 1000
+--5
+
